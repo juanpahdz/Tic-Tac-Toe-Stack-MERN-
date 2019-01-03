1c1abc1850f0388f130c4bf4d9c2f8a15c9f90ea
Juanpahdz01 <malbo@gmail.com>
2019-01-03 15:50:46 -0500

    server configurated

--- src/server.js
+++ src/server.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+const path = require('path')
 
 // setttings 
 app.set('port', process.env.PORT || 3000)
 
 // midlewares
+app.use(morgan('dev'))
+app.use(express.json())
 
 // Routes
+app.use(require('./routes/api.routes'))
 
 // static files
+app.use(express.static(path.join(__dirname, 'public')))
 
 // start app
 app.listen(app.get('port'), () => { console.log('server is ready in', app.get('port'))})
