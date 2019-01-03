ab0c4c3f92eb6d8e4089005235798a8ec08775f3
Juanpahdz01 <malbo@gmail.com>
2019-01-03 17:11:57 -0500

    rest api done

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,6 +1,45 @@
 const express = require('express')
-const router = express.Router
+const router = express.Router()
 
+const Board = require('../models/boards')
 
+// get the boards from the data base here 
+
+router.get('/api/boards', async (req, res) => { 
+    const boards = await Board.find()
+    res.json(boards)
+})
+
+// get an specific board
+
+router.get('/api/boards/:id', async (req, res) => {
+    const board = await Board.findById(req.params.id)
+    res.json(board)
+})
+
+// add new board from the game 
+
+router.post('/api/boards', async (req, res) => {
+    const { status, board } = req.body
+    const boards = new Board({status, board})
+    await boards.save()
+    res.json({status: 'the board was added'})
+})
+
+// re-play the game 
+
+router.put('/api/boards/:id', async (req, res) => {
+    const { status, board } = req.body;
+    const newBoard = {status, board}
+    await Board.findByIdAndUpdate(req.params.id, newBoard)
+    res.json({status: 'the game was recibed again'})
+})
+
+// delete the history
+
+router.delete('/api/boards', async (req, res) => {
+    await Board.remove()
+    res.json({status: 'the history was deleted'})
+})
 
 module.exports = router
