da3dfce3f8ace71030a0629d015fe49928dca280
Juanpahdz01 <malbo@gmail.com>
2019-01-04 12:57:59 -0500

    funcion click and restart were added

--- src/app/Componets/Counter.js
+++ src/app/Componets/Counter.js
@@ -1,13 +1,31 @@
 import React from 'react'
 
 const Counter = (props) => {
+ console.log(props)
+
+ let styleO = {
+    borderBottom: 'none'
+    }
+
+ let styleX = {
+    borderBottom: '5px solid #2bbbad'
+    }
+ 
+   if(props.turn == 'X'){
+       styleX.borderBottom = `5px solid #2bbbad`
+       styleO.borderBottom = 'none'
+   } else{
+       styleO.borderBottom = '5px solid #2bbbad'
+       styleX.borderBottom = `none`
+   }
+    
         return(
             <div className="container">
                 <div className="player-1">
                     <div className="row">
                         <h2 className="col-8">Player 1</h2>
                         <div className="col-4 identificator">
-                            <button className="btn-player btn-player-active btn">X</button>
+                            <button className="X btn-player btn" style={styleX}>X</button>
                         </div>
                     </div>
                 </div>
@@ -16,7 +34,7 @@ const Counter = (props) => {
                     <div className="row">
                         <h2 className="col-8">Player 2</h2>
                         <div className="col-4 identificator">
-                            <button className="btn-player btn">O</button>
+                            <button className="btn-player btn" style={styleO}>O</button>
                         </div>                                        
                     </div>
                 </div>
--- src/app/app.js
+++ src/app/app.js
@@ -1,17 +1,57 @@
 import React, { Component } from 'react'
 import Counter from './Componets/Counter.js'
+import uid from 'uid'
 
 class App extends Component{
     constructor(){
         super()
         this.state = {
             draws: '3',
+            totalMoves: 0,
             turn: 'X',
             status: 'inprogress',
-            board: '',
+            squares: '',
             message: '¿ Are we ready to start ?'
         }
+
+        this.boardState ={
+            board: Array(9).fill(''),
+        }
     }
+
+    componentWillMount() {
+        this.restart()
+    }
+    
+    clicked = (e) => {
+        let index = e.target.dataset.squares
+        if(this.boardState.board[index]  == '' ) {
+            this.boardState.board[index] = this.state.turn
+            e.target.innerText = this.state.turn
+    
+            this.setState({
+                turn: this.state.turn == 'X' ? 'O' : 'X',
+                totalMoves: this.state.totalMoves+1
+            })
+        }
+    }
+
+    restart = () => {
+        this.boardState.board = Array(9).fill('')
+        this.setState({
+            totalMoves: 0,
+            turn: 'X',
+            status: 'in progress',
+            message: '¿are we ready to start?',
+            squares: <div className="row bigboard" onClick={(e) => { this.clicked(e)}} >
+                        {this.boardState.board.map( (squares, key ) => {
+                            return <div className="square" data-squares={key} key={uid()}></div>
+                        })
+                        }
+                    </div>
+        })
+    }
+
     render(){
         return (
             <React.Fragment>
@@ -24,25 +64,14 @@ class App extends Component{
                         {/* col-3 */}
 
                         <div className="col-3 counter-panel">
-                            <Counter draws={this.state.draws}>
+                            <Counter draws={this.state.draws} turn={this.state.turn}>
                             </Counter>
                         </div>
 
                         {/* col-5 */}
 
                         <div className="col-5 game">
-                             <div className="row bigboard">
-                                {/* {this.state.quares} */}
-                                <div className="square">X</div>
-                                <div className="square">O</div>
-                                <div className="square"></div>
-                                <div className="square">X</div>
-                                <div className="square"></div>
-                                <div className="square"></div>
-                                <div className="square">O</div>
-                                <div className="square">X</div>
-                                <div className="square">O</div>
-                             </div>                       
+                            { this.state.squares }
                         </div>
 
                         {/* col-4 */}
@@ -50,7 +79,7 @@ class App extends Component{
                         <div className="col-4 historial">
                             <table>
                                 <thead>
-                                    <tr class="text-center" >
+                                    <tr className="text-center" >
                                         <th>Game</th>
                                         <th>Status</th>
                                     </tr>
@@ -81,7 +110,7 @@ class App extends Component{
 
                      <section>
                          <div className="control-panel">
-                            <button className="btn btn-secundary btn-panel">Restart</button>
+                            <button className="btn btn-secundary btn-panel" onClick={(e) => this.restart(e) }>Restart</button>
                             <button className="btn btn-secundary btn-panel">Pause</button>
                             <button className="btn btn-secundary btn-panel">Delete history</button>
                          </div>
