2db5bc2ce22a52ac668c7c6c0934e3ea0de0dccd
Juanpahdz01 <malbo@gmail.com>
2019-01-04 11:14:37 -0500

    styles Done

--- src/app/Componets/Counter.js
+++ src/app/Componets/Counter.js
@@ -7,20 +7,16 @@ const Counter = (props) => {
                     <div className="row">
                         <h2 className="col-8">Player 1</h2>
                         <div className="col-4 identificator">
-                            <button className="btn-player btn btn-secundary">X</button>
+                            <button className="btn-player btn-player-active btn">X</button>
                         </div>
                     </div>
                 </div>
 
-                <div className="text-center my-4">
-                    <h4>Draws <span className="draws">{ props.draws} </span></h4>
-                </div>
-          
                 <div className="player-2">
                     <div className="row">
                         <h2 className="col-8">Player 2</h2>
                         <div className="col-4 identificator">
-                            <button className="btn-player btn btn-secundary">O</button>
+                            <button className="btn-player btn">O</button>
                         </div>                                        
                     </div>
                 </div>
--- src/app/app.js
+++ src/app/app.js
@@ -15,7 +15,7 @@ class App extends Component{
     render(){
         return (
             <React.Fragment>
-                <div className="container App">
+                <div className="container app">
                     <div className="message text-center text-uppercase mb-5">
                         <h1> { this.state.message } </h1>
                     </div>
@@ -31,20 +31,49 @@ class App extends Component{
                         {/* col-5 */}
 
                         <div className="col-5 game">
-                             <div className="row justify-contet-center bigboard">
-                                {this.state.quares}
+                             <div className="row bigboard">
+                                {/* {this.state.quares} */}
+                                <div className="square">X</div>
+                                <div className="square">O</div>
+                                <div className="square"></div>
+                                <div className="square">X</div>
+                                <div className="square"></div>
+                                <div className="square"></div>
+                                <div className="square">O</div>
+                                <div className="square">X</div>
+                                <div className="square">O</div>
                              </div>                       
                         </div>
 
                         {/* col-4 */}
 
                         <div className="col-4 historial">
-                            <table className="table rounded">
-                                <thead className="thead-dark">
-                                    <tr className="col">Game</tr>
-                                    <tr className="col">Status</tr>
+                            <table>
+                                <thead>
+                                    <tr class="text-center" >
+                                        <th>Game</th>
+                                        <th>Status</th>
+                                    </tr>
                                 </thead>
-                                <tbody className="table-body">
+                                <tbody>
+                                 <tr>
+                                     <td>
+                                        <div className="board">
+                                            <div className="minisquare">x</div>
+                                            <div className="minisquare">O</div>
+                                            <div className="minisquare">x</div>
+                                            <div className="minisquare">x</div>
+                                            <div className="minisquare">X</div>
+                                            <div className="minisquare">x</div>
+                                            <div className="minisquare">x</div>
+                                            <div className="minisquare">O</div>
+                                            <div className="minisquare">O</div>
+                                        </div>
+                                     </td>
+                                     <td>
+                                        <button className="btn btn-finished"> Winer X</button>
+                                     </td>
+                                 </tr>
                                 </tbody>
                             </table>
                         </div>    
@@ -52,9 +81,9 @@ class App extends Component{
 
                      <section>
                          <div className="control-panel">
-                            <button className="btn btn-secundary btn-player-active btn-panel">Restart</button>
-                            <button className="btn btn-secundary btn-player-active btn-panel">Pause</button>
-                            <button className="btn btn-secundary btn-player-active btn-panel">Delete history</button>
+                            <button className="btn btn-secundary btn-panel">Restart</button>
+                            <button className="btn btn-secundary btn-panel">Pause</button>
+                            <button className="btn btn-secundary btn-panel">Delete history</button>
                          </div>
                      </section>    
                 </div>
